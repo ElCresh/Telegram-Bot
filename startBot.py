d58455bda8c77ec64de36c4d0948db3044c59056
Andrea Crescentini <cresh.it@gmail.com>
2015-09-14 20:24:02 +0200

    Migliorie al codice. Modifica del messaggio di benvenuto.

--- startBot.py
+++ startBot.py
@@ -16,9 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
 
-TELEGRAM_BOT_TOKEN=""
-
 import telegram
+
+TELEGRAM_BOT_TOKEN=""
+TELEGRAM_BOT_NAME="Test Bot"
 
 print ("Avvio bot in corso...")
 print ("Verifica autenticazione..")
@@ -52,16 +53,16 @@ while True:
 		chat_id = update.message.chat.id
 		update_id = update.update_id
 		
-		if text:
+		if (text):
 			print ("[MSG] "+text)
 			
-			if text == "Come ti chiami?":
+			if (text == "Come ti chiami?"):
 				reply="Di certo non Siri.... Ti pare?!?!?"
-			elif text == "Ti andrebbe un po' di schweppes solo io e te?":
+			elif (text == "Ti andrebbe un po' di schweppes solo io e te?"):
 				reply="No.... Ehi che ti aspettavi?"
 				bot.sendPhoto(chat_id=chat_id, photo='http://652af66dabe8673856dc500efee6dfde.s3.amazonaws.com/wp-content/uploads/2011/06/Uma_Thurman_Schweppes_2011-5.jpeg')
-			elif text == "/start":
-				reply="Benvenuto nel fighissimo bot di Cresh"
+			elif (text == "/start"):
+				reply="Ciao. Io sono "+TELEGRAM_BOT_NAME+". Piacere di conoscerti!"
 			else:
 				reply="Mi dispiace ma non capisco cosa intendi per: \""+text+"\""
 			bot.sendMessage(chat_id=chat_id, text=reply)
